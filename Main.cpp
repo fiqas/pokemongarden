099b216328b11e4bfdfd929aebfca93347f106ea
fiqas <salimaire@gmail.com>
2014-10-17 08:57:46 +0200

    Pliki
    Dodałam pliki.

---
+++ Main.cpp
@@ -0,0 +1,29 @@
+
+#include "stdafx.h"
+#include "GardenManager.h"
+
+
+int main(int argc, char* argv[])
+{
+	// get things going
+	//  optional parameters:
+	//		int windowWidth			default: 1024
+	//		int windowHeight		default: 768
+	//		std::string windowName	default: "Angel Engine"
+	//		bool antiAliasing		default: false
+	//		bool fullScreen			default: false
+	//		bool resizable			default: false
+	theWorld.Initialize();
+	theWorld.SetupPhysics(Vector2(0.0f, 0.0f));
+	theWorld.SetGameManager(new GardenManager());
+		
+	//YOUR GAME SETUP CODE HERE
+	
+	// do all your setup first, because this function won't return until you're exiting
+	theWorld.StartGame();
+	
+	// any cleanup can go here
+	theWorld.Destroy();
+	
+	return 0;
+}
