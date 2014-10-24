ed5be7241b22e6852d86307e45b8432f3bd16216
fiqas <salimaire@gmail.com>
2014-10-24 20:33:05 +0200

    Pikachu - chodzenie z czystym kodem
    Pikachu zmienia animacje w zależności od wysłanych Message. Teraz należy
    zrobić, by odbierał wiadomości od obiektów dookoła niego.

--- Pikachu.h
+++ Pikachu.h
@@ -5,8 +5,12 @@ public:
 	Pikachu();
 	virtual ~Pikachu();
 
+	float GoUpDown(float yVector, b2Vec2 currentVelocity);
+	float GoLeftRight(float xVector, b2Vec2 currentVelocity);
+
 	virtual void Update(float dt);
 	virtual void Render();
+	virtual void ReceiveMessage(Message* message);
 
 };
 
