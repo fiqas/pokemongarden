7b14ce9f7707f50f5ffcfbb7d0ef3749df61a0f8
fiqas <salimaire@gmail.com>
2014-10-24 08:50:16 +0200

    Uporządkowany Pikachu
    Pikachu chodzi - co prawda, jak się trzyma strzałki, to on nie mieli
    nogami, ale to najmniejszy problem póki co.

--- Pikachu.cpp
+++ Pikachu.cpp
@@ -1,8 +1,8 @@
 #include "stdafx.h"
 #include "Pikachu.h"
 
-//Do ogarniêcia, wklecenie ³adnej animacji jak Pikaczu siê rusza, dodatkowo niech uderza w pieñ drzewa a nie w ca³e, ¿eby to mia³o
-//rêce i nogi.
+//Do ogarniecia, wklecenie 3adnej animacji jak Pikaczu sie rusza, dodatkowo niech uderza w pien drzewa a nie w ca3e, ?eby to mia3o
+//rece i nogi.
 
 Pikachu::Pikachu(void) {
 
@@ -16,40 +16,34 @@ Pikachu::Pikachu(void) {
 	SetShapeType(PhysicsActor::SHAPETYPE_BOX);
 	SetPosition(0.0f, 0.0f);
 	SetLayer(3);
-	facing_front = false;
-	facing_back = true;
-	facing_right = false;
-	facing_left = false;
 	InitPhysics();
 
 }
 
 void Pikachu::Update(float dt) {	
 
+	PhysicsActor::Update(dt);
+
 	b2Vec2 currentVelocity = GetBody()->GetLinearVelocity(); // jak sama nazwa wskazuje.
 	Vector2 vel(currentVelocity.x, currentVelocity.y);
 	
-	float maxVel = theTuning.GetFloat("PikachuMaxSpeed"); // plik o nazwie tuning w configu, ³adnie zebrane ró¿ne zmienne
+	float maxVel = theTuning.GetFloat("PikachuMaxSpeed"); // plik o nazwie tuning w configu, 3adnie zebrane ró?ne zmienne
 	float xVector = 0.0f; 
 	float yVector = 0.0f;
 
 	if(theInput.IsKeyDown(ANGEL_KEY_RIGHTARROW)) {
 
-		facing_front = false;
-		facing_back = false;
-		facing_right = true;
-		facing_left = false;
-		xVector = 3.0f;
+		xVector = 2.0f;
+
+		PlaySpriteAnimation(0.1f, SAT_OneShot, 12, 15, "walkingRight"); 
 
 	}
 
 	if(theInput.IsKeyDown(ANGEL_KEY_LEFTARROW)) {
-		
-		facing_front = false;
-		facing_back = false;
-		facing_right = false;
-		facing_left = true;
-		xVector = -3.0f;
+
+		xVector = -2.0f;
+
+		PlaySpriteAnimation(0.1f, SAT_OneShot, 8, 11, "walkingLeft"); 
 
 	}
 
@@ -57,49 +51,21 @@ void Pikachu::Update(float dt) {
 	float desiredVelocity = xVector * maxVel;	
 	float velocityChangeX = desiredVelocity - currentVelocity.x;
 	float impulseX = GetBody()->GetMass() * velocityChangeX;
-
-	if (!IsSpriteAnimPlaying() && facing_left) {
-		PlaySpriteAnimation(0.1f, SAT_Loop, 8, 11, "walkingLeft");
-	}
-
-	if(IsSpriteAnimPlaying() && impulseX == 0.0f) {
-
-		SetSpriteFrame(9);
-
-	}
-
-	if (!IsSpriteAnimPlaying() && facing_right) {
-		PlaySpriteAnimation(0.1f, SAT_Loop, 12, 15, "walkingRight");
-	}
-
-	if (!IsSpriteAnimPlaying() && facing_front) {
-
-		std::cout << "front" << std::endl;
-		PlaySpriteAnimation(0.1f, SAT_Loop, 4, 7, "walkingUp"); 
-	}
-
-	if (!IsSpriteAnimPlaying() && facing_back) {
-		std::cout << "back" << std::endl;
-		PlaySpriteAnimation(0.1f, SAT_Loop, 0, 3, "walkingDown"); 
-	}
+	ApplyLinearImpulse(Vector2(impulseX, 0), GetPosition());
 
 	if(theInput.IsKeyDown(ANGEL_KEY_UPARROW)) {
-		yVector = 3.0f;
+	
+		yVector = 2.0f;
 
-		facing_front = true;
-		facing_back = false;
-		facing_right = false;
-		facing_left = false;
+		PlaySpriteAnimation(0.1f, SAT_OneShot, 4, 7, "walkingUp"); 
 
 	}
 
 	if(theInput.IsKeyDown(ANGEL_KEY_DOWNARROW)) {
-		yVector = -3.0f;
 
-		facing_front = false;
-		facing_back = true;
-		facing_right = false;
-		facing_left = false;
+		yVector = -2.0f;
+
+		PlaySpriteAnimation(0.1f, SAT_OneShot, 0, 3, "walkingDown"); 
 
 	}
 
@@ -107,35 +73,7 @@ void Pikachu::Update(float dt) {
 	float desiredVelocityY = yVector * maxVel;	
 	float velocityChangeY = desiredVelocityY - currentVelocity.y;
 	float impulseY = GetBody()->GetMass() * velocityChangeY;
-
-/*	if(impulseY == 0.0f && impulseX == 0.0f && facing_right) {
-
-		SetSpriteFrame(13);
-
-	}
-
-
-	if(impulseY == 0.0f && impulseX == 0.0f && facing_left) {
-
-		SetSpriteFrame(9);
-
-	}
-
-	if(impulseY == 0.0f && impulseX == 0.0f && facing_front) {
-
-		SetSpriteFrame(6);
-
-	}
-
-	if(impulseY == 0.0f && impulseX == 0.0f && facing_back) {
-
-		SetSpriteFrame(1);
-
-	}
-*/
-	ApplyLinearImpulse(Vector2(impulseX, 0), GetPosition());
 	ApplyLinearImpulse(Vector2(0, impulseY), GetPosition());
-	PhysicsActor::Update(dt);
 
 }
 
